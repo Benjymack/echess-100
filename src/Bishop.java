bcb1c8b0a4d5a42445ffb41dede06de40ab1ab39
Sofa <squishysparrow7.7@gmail.com>
2023-04-16 00:37:47 +1200

    Add Bishop polygons

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,5 +1,48 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Bishop extends ChessPiece {
     Bishop(int x, int y, ChessColor color) {
         super(x, y, color);
+        ArrayList<Polygon> polygons = new ArrayList<>();
+
+
+        // TODO: There's gotta be a DRY way of setting colors
+        Color outline;
+        Color fill;
+
+        if (color.equals(ChessColor.BLACK)) {
+            outline = Color.white;
+            fill = Color.black;
+        } else {
+            outline = Color.black;
+            fill = Color.white;
+        }
+
+        polygons.add(
+                new Polygon(
+                        new double[]{0.308, 0.67, 0.578, 0.636, 0.552, 0.572, 0.42, 0.468, 0.392, 0.456, 0.436, 0.372, 0.344, 0.42},
+                        new double[]{0.752, 0.752, 0.464, 0.346, 0.068, 0.032, 0.036, 0.068, 0.206, 0.344, 0.364, 0.246, 0.356, 0.46},
+                        14,
+                        outline,
+                        fill,
+                        getX() * getSize(),
+                        getY() * getSize(),
+                        getSize())
+        );
+
+        polygons.add(
+                new Polygon(
+                        new double[]{0.114, 0.108, 0.86, 0.848},
+                        new double[]{0.836, 0.928, 0.928, 0.836},
+                        4,
+                        outline,
+                        fill,
+                        getX() * getSize(),
+                        getY() * getSize(),
+                        getSize())
+        );
+
+        setPolygons(polygons);
     }
 }
