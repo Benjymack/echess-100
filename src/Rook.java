599689635187bee7f22d6db8e1c4565de885efe8
Sofa <squishysparrow7.7@gmail.com>
2023-04-16 00:24:25 +1200

    Add polygons for rook

--- src/Rook.java
+++ src/Rook.java
@@ -1,5 +1,48 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Rook extends ChessPiece {
     Rook(int x, int y, ChessColor color) {
         super(x, y, color);
+        ArrayList<Polygon> polygons = new ArrayList<>();
+
+
+        // TODO: There's gotta be a DRY way of setting colors
+        Color outline;
+        Color fill;
+
+        if (color.equals(ChessColor.BLACK)) {
+            outline = Color.white;
+            fill = Color.black;
+        } else {
+            outline = Color.black;
+            fill = Color.white;
+        }
+
+        polygons.add(
+                new Polygon(
+                        new double[]{0.116, 0.866, 0.866, 0.122},
+                        new double[]{0.888, 0.89, 0.73, 0.724},
+                        4,
+                        outline,
+                        fill,
+                        getX() * getSize(),
+                        getY() * getSize(),
+                        getSize())
+        );
+
+        polygons.add(
+                new Polygon(
+                        new double[]{0.222, 0.758, 0.702, 0.86, 0.894, 0.728, 0.71, 0.644, 0.658, 0.566, 0.566, 0.462, 0.468, 0.37, 0.37, 0.31, 0.302, 0.12, 0.164, 0.304},
+                        new double[]{0.654, 0.654, 0.292, 0.29, 0.086, 0.086, 0.172, 0.168, 0.08, 0.078, 0.164, 0.162, 0.084, 0.09, 0.158, 0.162, 0.078, 0.072, 0.284, 0.284},
+                        20,
+                        outline,
+                        fill,
+                        getX() * getSize(),
+                        getY() * getSize(),
+                        getSize())
+        );
+
+        setPolygons(polygons);
     }
 }
