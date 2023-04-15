80bbcde45be18a810de262693227c2ed5255df93
Sofa <squishysparrow7.7@gmail.com>
2023-04-15 17:47:50 +1200

    Base for program

---
+++ src/ChessPiece.java
@@ -0,0 +1,31 @@
+import ecs100.*;
+
+// Not sure if i'm doing abstract right
+public abstract class ChessPiece {
+    private static double size = 20;
+    private final ChessColor color;
+    private int x;
+    private int y;
+
+    // Dimensions:
+    //  1: Groups
+    //  2: Points
+    //  3: X,Y
+    // Should be set by the subclass
+    private double[][][] points;
+
+
+    ChessPiece(int x, int y, ChessColor color) {
+        this.color = color;
+
+        this.y = y;
+        this.x = x;
+    }
+
+    public abstract void draw();
+
+    public void setSize(double new_size) {
+        // TODO: Checks
+        size = new_size;
+    }
+}
